f2560779ff0cc0a2beac7c7ef7f64bee0c9add49
Connor <connor@Connors-MacBook-Pro-2.local>
2017-06-24 15:51:01 -0400

    test two key value pairs

--- src/test/scala/AkkademyDbSpec.scala
+++ src/test/scala/AkkademyDbSpec.scala
@@ -10,11 +10,21 @@ class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
   implicit val system = ActorSystem()
   describe("akkademyDb") {
     describe("given SetRequest") {
-      it("should place key/value into map"){
+      it("should place key/value into map") {
         val actorRef = TestActorRef(new AkkademyDb)
         actorRef ! SetRequest("key", "value")
         val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(Some("value"))
+      }
+    }
+    describe("given two SetRequests") {
+      it("should place two key/values into map") {
+        val actorRef = TestActorRef(new AkkademyDb)
+        actorRef ! SetRequest("key", "value")
+        actorRef ! SetRequest("key2", "value2")
+        val akkademyDb = actorRef.underlyingActor
+        akkademyDb.map.get("key") should equal(Some("value"))
+        akkademyDb.map.get("key2") should equal(Some("value2"))
       }
     }
   }
